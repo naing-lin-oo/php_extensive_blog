a85c1ed2e6de4778b9217a20fb01c6ed90afd340
naing-lin-oo <nainglinoo19497@gmail.com>
2020-11-23 10:21:12 +0630

    user delete bug sloving

--- admin/user_delete.php
+++ admin/user_delete.php
@@ -5,6 +5,9 @@ require '../config/config.php';
 $pdostmt=$pdo->prepare("DELETE FROM users WHERE id=".$_GET['id']);
 $pdostmt->execute();
 
+$pdostmt2=$pdo->prepare("DELETE FROM comments WHERE author_id=".$_GET['id']);
+$pdostmt2->execute();
+
 header('Location: user_list.php');
 
 ?>
